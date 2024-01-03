c11d64fb8461bf9f82c2e73fb9051547aaf3e308
Aditya @ArchLinux <132184385+adityadeshlahre@users.noreply.github.com>
2024-01-03 22:31:15 +0530

    signup component makeup

--- frontend/src/components/Signin.tsx
+++ frontend/src/components/Signin.tsx
@@ -44,11 +44,61 @@ export const Signin = () => {
     
     }
     
-    return <div>
-        <button onClick={() => {
-            onSignin()
-        }}>
-            Login with google
-        </button>
-    </div>
-}
+    return (
+      <div className="flex items-center justify-center h-screen bg-gradient-to-r from-neutral-500 to-neutral-900">
+        <div className="w-[30%] flex items-center justify-center relative">
+          <div className="absolute top-1/2 left-0 transform -translate-y-1/2 mt-4 ml-4">
+            <img
+              src="https://upload.wikimedia.org/wikipedia/commons/1/19/LeetCode_logo_black.png"
+              alt="Logo"
+              className="h-80 w-80"
+            />
+            <h1 className="text-3xl font-bold mb-2 text-center text-cyan-50">
+              LeetClone Serverless Arch <br /> built on firebase
+            </h1>
+          </div>
+          <div className="border-r border-gray-400 h-full mx-4"></div>
+        </div>
+  
+        <div className="bg-white p-8 rounded shadow-md">
+          <img
+            src="https://upload.wikimedia.org/wikipedia/commons/1/19/LeetCode_logo_black.png"
+            alt="Logo"
+            className="mx-auto mb-4 h-16"
+          />
+  
+          <h1 className="text-3xl font-bold mb-8 text-center text-gray-800">
+            Welcome Back!
+          </h1>
+          <button
+            onClick={() => {
+              onSignin();
+            }}
+            className="bg-neutral-500 hover:bg-neutral-800 text-white py-2 px-4 rounded-full w-full"
+          >
+            <span className="flex items-center justify-center">
+              Login with Google
+            </span>
+          </button>
+          <div className="mt-4 text-center">
+            <button
+              onClick={() => {
+                onSignin();
+              }}
+              className="bg-white p-2 rounded-full"
+            >
+              <img src="../public/google.png" alt="Google" className="h-8 w-8" />
+            </button>
+            <button
+              onClick={() => {
+                onSignin();
+              }}
+              className="bg-white p-2 rounded-full"
+            >
+              <img src="../public/github.png" alt="GitHub" className="h-8 w-8" />
+            </button>
+          </div>
+        </div>
+      </div>
+    );
+  };
