559eec2753f41b5feecdbc6a278cec8a9f8a2a84
its-id <ik.dana24@gmail.com>
2024-01-03 22:02:00 +0530

    Create Signin Page UI

--- frontend/src/components/Signin.tsx
+++ frontend/src/components/Signin.tsx
@@ -2,6 +2,8 @@ import { getAuth, sendSignInLinkToEmail, signInWithPopup } from "firebase/auth";
 import { useState } from "react";
 import { auth } from "../App";
 import { GoogleAuthProvider } from "firebase/auth";
+import GoogleIcon from "../assets/google.svg";
+import GithubIcon from "../assets/github.svg";
 
 const provider = new GoogleAuthProvider();
 
@@ -43,12 +45,34 @@ export const Signin = () => {
 
     
     }
-    
-    return <div>
-        <button onClick={() => {
-            onSignin()
-        }}>
-            Login with google
-        </button>
+
+  return (
+    <div className='min-h-[100vh] flex flex-col justify-center py-12 sm:px-6 lg:px-8 bg-black'>
+      <div className='mt-8 sm:mx-auto sm:w-full sm:max-w-md'>
+        <div className='bg-white py-12 px-4 shadow sm:rounded-lg sm:px-10'>
+          <div className='flex flex-col items-center justify-center gap-4'>
+            <p className='font-normal text-2xl text-gray-900'>Welcome</p>
+
+            <p className='font-light text-sm text-gray-600'>
+              Log in to continue to DailyCode.
+            </p>
+            <button
+              type='submit'
+              className='w-full flex justify-center items-center gap-2 py-3 px-4 border rounded font-light text-md hover:bg-gray-200 focus:outline-none focus:ring-2 '
+              onClick={() => onSignin()}
+            >
+              <img src={GoogleIcon} className='w-5 h-5 mr-2' />
+              Continue with Google
+            </button>
+            <button
+              className='w-full flex justify-center items-center gap-2 py-3 px-4 border rounded font-light text-md hover:bg-gray-200 focus:outline-none focus:ring-2 -mt-2'
+            >
+              <img src={GithubIcon} className='w-5 h-5 mr-2' />
+              Continue with Github
+            </button>
+          </div>
+        </div>
+      </div>
     </div>
-}
+  );
+};
