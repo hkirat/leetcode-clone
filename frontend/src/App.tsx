4f9b09e4f89a074dd1166ce975bb29acf7a22cb4
jukha <junaid.apsc@gmail.com>
2024-01-03 21:19:35 +0500

    Add content to landing page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,13 @@
-import './App.css'
-import { Landing } from './components/Landing'
+import "./App.css";
+import { Landing } from "./components/Landing";
 import { initializeApp } from "firebase/app";
-import { Signin } from './components/Signin';
-import { getAuth , onAuthStateChanged } from 'firebase/auth';
-import { useEffect } from 'react';
-import { RecoilRoot, useRecoilState, useSetRecoilState } from 'recoil';
-import { userAtom } from './store/atoms/user';
-import { Topbar } from './components/Topbar';
-import { Card } from './components/Card';
+import { Signin } from "./components/Signin";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
+import { useEffect } from "react";
+import { RecoilRoot, useRecoilState, useSetRecoilState } from "recoil";
+import { userAtom } from "./store/atoms/user";
+import { Topbar } from "./components/Topbar";
+import { Card } from "./components/Card";
 
 const firebaseConfig = {
   apiKey: "AIzaSyAjjsbl9eSDWSmfrWpFPap2uGuwONZ2N4g",
@@ -16,56 +16,89 @@ const firebaseConfig = {
   storageBucket: "leetcode-clone-c39eb.appspot.com",
   messagingSenderId: "66814187798",
   appId: "1:66814187798:web:a6b3702e191448722dd837",
-  measurementId: "G-ET5FNB5WCN"
+  measurementId: "G-ET5FNB5WCN",
 };
 
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 function App() {
-  return <RecoilRoot>
-    <StoreApp />
-  </RecoilRoot> 
+  return (
+    <RecoilRoot>
+      <StoreApp />
+    </RecoilRoot>
+  );
 }
 
 function StoreApp() {
   const [user, setUser] = useRecoilState(userAtom);
 
   useEffect(() => {
-    onAuthStateChanged(auth, function(user) {
+    onAuthStateChanged(auth, function (user) {
       if (user && user.email) {
         setUser({
           loading: false,
           user: {
-            email: user.email
-          }
-        })
+            email: user.email,
+          },
+        });
       } else {
         setUser({
           loading: false,
-        })
+        });
         // No user is signed in.
-        console.log('There is no logged in user');
+        console.log("There is no logged in user");
       }
     });
-  }, [])
+  }, []);
 
   if (user.loading) {
-    return <div>loading ...</div>
+    return <div>loading ...</div>;
   }
-  
+
   if (!user.user) {
-    return <div><Signin /></div>
+    return (
+      <div>
+        <Signin />
+      </div>
+    );
   }
-  
+
   return (
-    <div className="place-items-center grid"> 
+    <div className="place-items-center grid">
       <div className="max-w-screen-lg w-full">
         <Topbar />
-        <Card>hi there</Card>
+        <div className="mt-4 grid grid-cols-[1fr_2fr_1fr] gap-4">
+          <aside>
+            <div className="mb-4">
+              <Card>
+                Start following your friends on Erdős to view their activity
+                feed on the homepage.
+              </Card>
+            </div>
+            <Card>
+              <h3 className="text-center text-2xl font-normal mb-8">
+                Progress
+              </h3>
+
+              <p>The secret of getting ahead is getting started.</p>
+            </Card>
+          </aside>
+          <main>
+            <Card>
+              <h3 className="text-2xl font-normal mb-8">Latest Problems</h3>
+              <div>
+                
+              </div>
+            </Card>
+          </main>
+          <aside>
+            <Card>hi</Card>
+          </aside>
+        </div>
       </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
