7c938a34e6097ed0ebf1453b3fb7f6c32d93730d
harkirat <harkirat.iitr@gmail.com>
2024-01-03 21:25:56 +0530

    Added components to build

---
+++ frontend/src/components/LeaderBoard.tsx
@@ -0,0 +1,7 @@
+// see https://erdos.sdslabs.co/users
+export const Leaderboard = ({leaderboard}: {
+    leaderboard: {image: string; name: string; points: number}[]
+}) => {
+    return <div>
+    </div>
+}
