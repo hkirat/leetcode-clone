bd9f8418461f416ac628beaa31911cc56dc73ebd
iqbal <111554475+noobcoder6602@users.noreply.github.com>
2024-01-03 22:38:52 +0530

    Refactor Signin component UI

--- frontend/src/components/Signin.tsx
+++ frontend/src/components/Signin.tsx
@@ -6,12 +6,12 @@ import { GoogleAuthProvider } from "firebase/auth";
 const provider = new GoogleAuthProvider();
 
 const actionCodeSettings = {
-// URL you want to redirect back to. The domain (www.example.com) for this
-  // URL must be in the authorized domains list in the Firebase Console.
-  url: 'http://localhost:5173',
-  // This must be true.
-  handleCodeInApp: true,
-  };
+    // URL you want to redirect back to. The domain (www.example.com) for this
+    // URL must be in the authorized domains list in the Firebase Console.
+    url: 'http://localhost:5173',
+    // This must be true.
+    handleCodeInApp: true,
+};
 
 export const Signin = () => {
     const [email, setEmail] = useState("");
@@ -41,14 +41,30 @@ export const Signin = () => {
                 // ...
             });
 
-    
+
     }
-    
+
     return <div>
-        <button onClick={() => {
-            onSignin()
-        }}>
-            Login with google
-        </button>
+        <div className="h-screen md:flex bg-gray-900 text-white">
+            <div className="flex w-full md:w-1/2 justify-around py-10 items-center bg-gradient-to-tr from-purple-800 to-indigo-900">
+                <div>
+                    <img src="../../public/logo.png" alt="Daily Code Logo" className="h-16 mb-4" />
+                    <h1 className="text-white font-bold text-4xl font-sans">Daily Code</h1>
+                    <p className="text-white mt-1">A New Way to Learn</p>
+                    <button type="submit" className="block w-28 bg-white text-indigo-800 mt-4 py-2 rounded-2xl font-bold mb-2">Read More</button>
+                </div>
+            </div>
+            <div className="flex md:w-1/2 justify-center py-10 items-center bg-gray-800">
+                <div className="bg-gray-800">
+                    <h1 className="text-gray-300 font-bold text-2xl mb-1">Hello Again!</h1>
+                    <p className="text-sm font-normal text-gray-500 mb-7">Welcome Back</p>
+                    <button onClick={() => { onSignin() }} type="submit" className="block w-full bg-indigo-600 mt-4 py-2 px-4 rounded-2xl text-white font-semibold mb-2">
+                        Login with Google
+                    </button>
+                    <span className="text-sm ml-2 hover:text-blue-500 cursor-pointer">Forgot Password?</span>
+                </div>
+            </div>
+        </div>
+
     </div>
 }
