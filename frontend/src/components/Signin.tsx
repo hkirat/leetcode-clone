b59f4cc343eea6cdf391b87fa96ee60f3441d52d
myj009 <mohammad.jagora@deliveroo.com>
2024-01-03 21:58:53 +0530

    Second commit

--- frontend/src/components/Signin.tsx
+++ frontend/src/components/Signin.tsx
@@ -18,6 +18,7 @@ import logo from "../assets/leetcode.svg";
 import google from "../assets/google.svg";
 import github from "../assets/github.svg";
 import { Separator } from "./ui/separator";
+import backdrop from "../assets/backdrop.png";
 
 const provider = new GoogleAuthProvider();
 
@@ -32,7 +33,6 @@ const actionCodeSettings = {
 export const Signin = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  const [isLoading, setIsLoading] = useState(false);
 
   async function onSignin() {
     signInWithPopup(auth, provider)
@@ -62,58 +62,62 @@ export const Signin = () => {
   }
 
   return (
-    <div className="h-screen flex justify-center items-center">
-      <Card className="flex flex-col items-center w-96">
-        <CardHeader className="flex flex-col items-center">
-          <CardTitle>
-            <img src={logo} alt="logo" width={50} height={50} />
-          </CardTitle>
-          <CardDescription>BEATCODE</CardDescription>
-        </CardHeader>
-        <CardContent className="flex flex-col gap-3 w-full items-center">
-          <Input
-            placeholder="E-mail"
-            type="email"
-            value={email}
-            onChange={(e) => {
-              setEmail(e.target.value);
-            }}
-          />
-          <Input
-            placeholder="Password"
-            type="password"
-            value={password}
-            onChange={(e) => {
-              setPassword(e.target.value);
-            }}
-          />
-          <Button className="mt-4" disabled={isLoading} onClick={onSignin}>
-            {isLoading && <Loader2 className="mr-2 h-4 w-4 animate-spin" />}
-            Sign in
-          </Button>
-          <Label>
-            Do not have an account?{" "}
-            <a className="text-[#FFA123]" href="/signup">
-              Sign up
-            </a>
-          </Label>
-        </CardContent>
-        <div className="flex space-x-4 justify-between items-center mb-3">
-          <Button variant="outline" size="icon" className="rounded-full p-1">
-            <img src={google} alt="Google" className="h-10 w-10" />
-          </Button>
-          <Button variant="outline" size="icon" className="rounded-full p-1">
-            <img src={github} alt="Github" className="h-10 w-10" />
-          </Button>
-        </div>
-        <Separator className="mx-3" />
-        <CardFooter className="mt-3">
-          <p className="flex justify-center">
-            This site is protected by reCAPTCHA and the Google Privacy Policy
-            and Terms of Service apply.
-          </p>
-        </CardFooter>
-      </Card>
+    <div className="flex w-full">
+      <div className="w-[30rem] hidden md:flex">
+        <img src={backdrop} alt="" />
+      </div>
+      <div className="h-screen flex flex-1 justify-center items-center">
+        <Card className="flex flex-col items-center w-80 sm:w-96">
+          <CardHeader className="flex flex-col items-center">
+            <CardTitle>
+              <img src={logo} alt="logo" width={50} height={50} />
+            </CardTitle>
+            <CardDescription>BEATCODE</CardDescription>
+          </CardHeader>
+          <CardContent className="flex flex-col gap-3 w-full items-center">
+            <Input
+              placeholder="E-mail"
+              type="email"
+              value={email}
+              onChange={(e) => {
+                setEmail(e.target.value);
+              }}
+            />
+            <Input
+              placeholder="Password"
+              type="password"
+              value={password}
+              onChange={(e) => {
+                setPassword(e.target.value);
+              }}
+            />
+            <Button className="mt-4" onClick={onSignin}>
+              Sign in
+            </Button>
+            <Label>
+              Do not have an account?{" "}
+              <a className="text-[#FFA123]" href="/signup">
+                Sign up
+              </a>
+            </Label>
+          </CardContent>
+          <div className="flex space-x-4 justify-between items-center mb-3">
+            <Button variant="outline" size="icon" className="rounded-full p-1">
+              <img src={google} alt="Google" className="h-10 w-10" />
+            </Button>
+            <Button variant="outline" size="icon" className="rounded-full p-1">
+              <img src={github} alt="Github" className="h-10 w-10" />
+            </Button>
+          </div>
+          <Separator className="mx-3" />
+          <CardFooter className="mt-3">
+            <p className="flex justify-center">
+              This site is protected by reCAPTCHA and the Google Privacy Policy
+              and Terms of Service apply.
+            </p>
+          </CardFooter>
+        </Card>
+      </div>
     </div>
   );
 };
