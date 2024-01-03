9018ed9e1a9cec251f9c70a3505429abf04aa4cd
Moh1tsingh <thakurmohitsingh2003@gmail.com>
2024-01-03 23:20:50 +0530

    improvements

--- frontend/src/components/ProblemList.tsx
+++ frontend/src/components/ProblemList.tsx
@@ -3,7 +3,19 @@
 const ProblemList = () => {
   const data = [
     {
-      question: "2 sum",
+      question: "Minimum Value",
+      solverImg:
+        "https://static.vecteezy.com/system/resources/thumbnails/019/879/186/small/user-icon-on-transparent-background-free-png.png",
+      topics: ["array", "hashmap"],
+    },
+    {
+      question: "Equation",
+      solverImg:
+        "https://static.vecteezy.com/system/resources/thumbnails/019/879/186/small/user-icon-on-transparent-background-free-png.png",
+      topics: ["array", "hashmap"],
+    },
+    {
+      question: "Largest Number",
       solverImg:
         "https://static.vecteezy.com/system/resources/thumbnails/019/879/186/small/user-icon-on-transparent-background-free-png.png",
       topics: ["array", "hashmap"],
@@ -15,43 +27,31 @@ const ProblemList = () => {
       topics: ["array", "hashmap"],
     },
     {
-      question: "2 sum",
+      question: "3 Musketeers",
       solverImg:
         "https://static.vecteezy.com/system/resources/thumbnails/019/879/186/small/user-icon-on-transparent-background-free-png.png",
       topics: ["array", "hashmap"],
     },
     {
-      question: "2 sum",
-      solverImg:
-        "https://static.vecteezy.com/system/resources/thumbnails/019/879/186/small/user-icon-on-transparent-background-free-png.png",
-      topics: ["array", "hashmap"],
-    },
-    {
-      question: "2 sum",
-      solverImg:
-        "https://static.vecteezy.com/system/resources/thumbnails/019/879/186/small/user-icon-on-transparent-background-free-png.png",
-      topics: ["array", "hashmap"],
-    },
-    {
-      question: "2 sum",
+      question: "Exponentiation",
       solverImg:
         "https://static.vecteezy.com/system/resources/thumbnails/019/879/186/small/user-icon-on-transparent-background-free-png.png",
       topics: ["array", "hashmap"],
     },
   ];
   return (
-    <div className=" shadow">
-      <h1 className=" text-2xl my-2 ml-2">All Problems</h1>
+    <div className=" shadow-lg my-4">
+      <h1 className=" text-2xl my-2 ml-4">All Problems</h1>
       {data.map((item, index) => (
-        <div className="flex my-4 text-lg bg-white shadow-sm py-4 px-2 border-b-2">
-          <div className=" pr-2 text-slate-400">#{index + 1}</div>
-          <div className=" ml-10 gap-4">
-            <div className=" ">{item.question}</div>
+        <div className="flex my-4 text-lg bg-white shadow-sm py-4 px-2 border-b-2 ">
+          <div className=" pr-2 text-slate-500">#{index + 1}</div>
+          <div className=" ml-10 ">
+            <div onClick={()=>{}} className="cursor-pointer">{item.question}</div>
 
             {item.topics.map((topic, topicIndex) => (
-              <span
+              <span onClick={()=>{}}
                 key={topicIndex}
-                className=" bg-gray-600 text-white px-3 mr-1"
+                className="cursor-pointer rounded py-1 bg-gray-600 text-white px-3 mr-1"
               >
                 {topic}
               </span>
@@ -60,7 +60,7 @@ const ProblemList = () => {
 
           <div className=" ml-80 flex flex-row p-2 text-sm gap-2 align-middle">
             <span>recently solved by</span>
-            <img className=" h-8" src={item.solverImg} alt="Image of Solver" />
+            <img onClick={()=>{}} className="cursor-pointer h-8" src={item.solverImg} alt="Image of Solver" />
           </div>
         </div>
       ))}
